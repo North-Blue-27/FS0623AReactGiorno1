e0a4768fd573aac6a83fca5f1582f163420caaf2
Casalena Antonio <casalenaantonio@hotmail.it>
2023-12-19 17:44:46 +0100

    mod funzione categoria

--- epibooks/src/components/AllTheBooks.jsx
+++ epibooks/src/components/AllTheBooks.jsx
@@ -6,49 +6,52 @@ import thrillerData from '../data/history.json';
 import romanceData from '../data/romance.json';
 import scifiData from '../data/scifi.json';
 
+
 const AllTheBooks = () => {
-  const [books, setBooks] = useState(horrorData);
-
-  const changeCategory = (category) => {
-    switch (category) {
-      case 'horror':
-        setBooks(horrorData);
-        break;
-      case 'fantasy':
-        setBooks(fantasyData);
-        break;
-      case 'thriller':
-        setBooks(thrillerData);
-        break;
-      case 'romance':
-        setBooks(romanceData);
-        break;
-      case 'scifi':
-        setBooks(scifiData);
-        break;
-      default:
-        setBooks(horrorData);
-    }
-  };
-
-  return (
-    <Container>
-      <Row className="justify-content-between align-items-center mb-3">
-        <Col xs="auto">
-          <h2>Books</h2>
-        </Col>
-        <Col xs="auto">
-          <DropdownButton id="dropdown-basic-button" title="Select Category">
-            <Dropdown.Item onClick={() => changeCategory('horror')}>Horror</Dropdown.Item>
-            <Dropdown.Item onClick={() => changeCategory('fantasy')}>Fantasy</Dropdown.Item>
-            <Dropdown.Item onClick={() => changeCategory('thriller')}>Thriller</Dropdown.Item>
-            <Dropdown.Item onClick={() => changeCategory('romance')}>Romance</Dropdown.Item>
-            <Dropdown.Item onClick={() => changeCategory('scifi')}>Scifi</Dropdown.Item>
-          </DropdownButton>
-        </Col>
-      </Row>
-      
-      <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+    const [books, setBooks] = useState(horrorData);
+    const [currentCategory, setCurrentCategory] = useState('horror'); // Imposta la categoria iniziale
+  
+    const changeCategory = (category) => {
+      setCurrentCategory(category); // Aggiorna la categoria corrente
+      switch (category) {
+        case 'horror':
+          setBooks(horrorData);
+          break;
+        case 'fantasy':
+          setBooks(fantasyData);
+          break;
+        case 'thriller':
+          setBooks(thrillerData);
+          break;
+        case 'romance':
+          setBooks(romanceData);
+          break;
+        case 'scifi':
+          setBooks(scifiData);
+          break;
+        default:
+          setBooks(horrorData);
+      }
+    };
+  
+    return (
+      <Container>
+        <Row className="justify-content-between align-items-center mb-3">
+          <Col xs="auto">
+            <h2>{currentCategory.charAt(0).toUpperCase() + currentCategory.slice(1)} Books</h2>
+          </Col>
+          <Col xs="auto">
+            <DropdownButton id="dropdown-basic-button" title="Select Category">
+              <Dropdown.Item onClick={() => changeCategory('horror')}>Horror</Dropdown.Item>
+              <Dropdown.Item onClick={() => changeCategory('fantasy')}>Fantasy</Dropdown.Item>
+              <Dropdown.Item onClick={() => changeCategory('thriller')}>Thriller</Dropdown.Item>
+              <Dropdown.Item onClick={() => changeCategory('romance')}>Romance</Dropdown.Item>
+              <Dropdown.Item onClick={() => changeCategory('scifi')}>Sci-Fi</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+        </Row>
+        
+        <Row xs={1} md={2} lg={3} xl={4} className="g-4">
         {books.map((book) => (
           <Col key={book.asin}>
           <Card style={{ height: '100%', backgroundColor: '#17192b', color: '#781c77' }}>
