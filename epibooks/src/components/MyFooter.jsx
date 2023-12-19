41cf9d45dd4fb2ef4ef261b5023f514d8c5e9f30
Casalena Antonio <casalenaantonio@hotmail.it>
2023-12-19 15:32:53 +0100

    footer

---
+++ epibooks/src/components/MyFooter.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyFooter = () => {
+  return (
+    <footer className="footer" style={{ backgroundColor: 'Gray', textAlign: 'center', color: 'White' }}>
+      <div className="container">
+        <span className="text-muted">© 2023 EpiBook. All rights reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default MyFooter;
